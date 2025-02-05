d82cdcdcb7f53bfbcae1b3ee7fd93f1d903d66db
baharshira <131886427+baharshira@users.noreply.github.com>
2025-02-05 02:48:35 -0500

    Add files via upload

---
+++ utils/apiFeatures.js
@@ -0,0 +1,57 @@
+class APIFeatures {
+    constructor(query, queryString) {
+        // Initialize the class with a Mongoose query and the request query string
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    filter() {
+        const queryObj = { ...this.queryString };
+        // Define fields to be excluded from filtering
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        // Remove excluded fields from the copied query object
+        excludedFields.forEach(el => delete queryObj[el]);
+
+        // 1B) Advanced filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        // Apply the filtered query to the class query property
+        this.query = this.query.find(JSON.parse(queryStr));
+
+        return this;
+    }
+
+    sort() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-createdAt');
+        }
+
+        return this;
+    }
+
+    limitFields() {
+        if (this.queryString.fields) {
+            const fields = this.queryString.fields.split(',').join(' ');
+            this.query = this.query.select(fields);
+        } else {
+            this.query = this.query.select('-__v');
+        }
+
+        return this;
+    }
+
+    paginate() {
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this;
+    }
+}
+module.exports = APIFeatures;
