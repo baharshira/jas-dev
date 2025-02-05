2f6c0cfa1f87b0a9a68f2da37bf2bd3557aa2fe7
baharshira <131886427+baharshira@users.noreply.github.com>
2025-02-05 02:27:43 -0500

    Delete routes directory

--- routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cartController = require('../controllers/cartController');
-const authController = require('../controllers/authController');
-
-const router = express.Router();
-
-router.get('/checkout-session/:id', authController.protect, cartController.getCheckoutSession)
-module.exports = router;
