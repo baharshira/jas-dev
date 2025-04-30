03a903457ee1e7afb618d5b6ab96d7bd322100d7
baharshira <131886427+baharshira@users.noreply.github.com>
2025-04-30 04:50:58 -0400

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,7 +6,7 @@ const handleCastErrorDB = err => {
 };
 
 const handleDuplicateFieldsDB = err => {
-    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; //regex for verifying the fields
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; 
     console.log(value);
 
     const message = `Duplicate field value: ${value}. Please use another value!`;
@@ -27,7 +27,6 @@ const handleJWTExpiredError = () =>
     new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, req, res) => {
-    // A) API
     if (req.originalUrl.startsWith('/api')) {
         return res.status(err.statusCode).json({
             status: err.status,
@@ -37,7 +36,6 @@ const sendErrorDev = (err, req, res) => {
         });
     }
 
-    // B) RENDERED WEBSITE
     console.error('ERROR ', err);
     return res.status(err.statusCode).render('error', {
         title: 'Something went wrong!',
@@ -46,7 +44,6 @@ const sendErrorDev = (err, req, res) => {
 };
 
 const sendErrorProd = (err, req, res) => {
-    // A) API
     if (req.originalUrl.startsWith('/api')) {
         // A) Operational, trusted error: send message to client
         if (err.isOperational) {
@@ -55,18 +52,15 @@ const sendErrorProd = (err, req, res) => {
                 message: err.message
             });
         }
-        // B) Programming or other unknown error: don't leak error details
-        // 1) Log error
+
         console.error('ERROR ', err);
-        // 2) Send generic message
         return res.status(500).json({
             status: 'error',
             message: 'Something went very wrong!'
         });
     }
 
-    // B) RENDERED WEBSITE
-    // A) Operational, trusted error: send message to client
+
     if (err.isOperational) {
         console.log(err);
         return res.status(err.statusCode).render('error', {
@@ -74,10 +68,9 @@ const sendErrorProd = (err, req, res) => {
             msg: err.message
         });
     }
-    // B) Programming or other unknown error: don't leak error details
-    // 1) Log error
+
     console.error('ERROR ', err);
-    // 2) Send generic message
+
     return res.status(err.statusCode).render('error', {
         title: 'Something went wrong!',
         msg: 'Please try again later.'
@@ -85,7 +78,7 @@ const sendErrorProd = (err, req, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-    // console.log(err.stack);
+    console.log(err.stack);
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
